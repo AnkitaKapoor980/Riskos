08cbe3587f65c3f46dd0265cae2fb63f1c107351
AnkitaKapoor980 <150882877+AnkitaKapoor980@users.noreply.github.com>
2025-03-08 16:05:39 +0530

    Real Time Stock Data API

---
+++ Real Time Stocks/Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import StockData from './StockData';
+
+function App() {
+    return (
+        <div>
+            <StockData symbol="RELIANCE.NS" />
+        </div>
+    );
+}
+
+export default App;
